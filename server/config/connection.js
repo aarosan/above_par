108a8f9c6c93b9529fe876d987d2d7a746059d20
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-10-07 17:38:34 -0500

    Updated connection.js with process.env.MONGODB_URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,27 +1,15 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
+require('dotenv').config(); 
 
-// Define the database name
-const dbName = 'exampleDatabase';
+const dbUri = process.env.MONGODB_URI;
+console.log('dbUri:', dbUri);
 
-// Get the MongoDB URI from environment variables or use a default value
-const mongoHost = process.env.MONGO_HOST || '127.0.0.1';
-const mongoPort = process.env.MONGO_PORT || '27017';
-const mongoUser = process.env.MONGO_USER || '';
-const mongoPass = process.env.MONGO_PASS || '';
-const mongoDbName = process.env.MONGO_DB_NAME || dbName;
-
-// Construct the MongoDB URI
-let mongoURI = `mongodb://${mongoHost}:${mongoPort}/${mongoDbName}`;
-if (mongoUser && mongoPass) {
-  mongoURI = `mongodb://${mongoUser}:${mongoPass}@${mongoHost}:${mongoPort}/${mongoDbName}`;
+if (!dbUri) {
+  throw new Error('MONGODB_URI environment variable is not set.');
 }
 
-connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('MongoDB connected successfully');
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err);
-  });
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
-module.exports = connection;
+module.exports = mongoose.connection;
