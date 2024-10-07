8180de6db060d49f3f612bb3d25a62e5ed0bd505
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-10-07 17:41:51 -0500

    Added middleware directory in server and authentication middleware in auth.js

---
+++ server/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config(); 
+const JWT_SECRET = process.env.JWT_SECRET; 
+
+// Middleware for token validation with logging
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  // console.log('Received token:', token); // Debugging line
+
+  if (!token) {
+    console.log('No token provided'); // Debugging line
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      console.log('Token verification failed:', err.message); // Debugging line
+      return res.sendStatus(403);
+    }
+    
+    // console.log('Token verified, user:', user); // Debugging line
+    req.userId = user.userId; // Set req.userId from token payload
+    next();
+  });
+};
+
+module.exports = authenticateToken;
