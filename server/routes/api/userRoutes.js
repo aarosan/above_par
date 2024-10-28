29bd4255ee40525e3ccb529466d92534694760a5
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-10-28 18:11:55 -0500

    Imported game and course controllers and defined GET & POST routes for authorized users

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../../controllers/userController');
+const { getCourses, createCourse } = require('../../controllers/courseController');
+const { getGames, createGame } = require('../../controllers/gameController');
 const authenticateToken = require('../../middleware/auth');
 
 // User routes
@@ -8,5 +10,13 @@ router.post('/signup', signup);
 router.post('/login', login);
 
 router.use(authenticateToken); // Authenticate for all following routes
+
+// Course routes
+router.get('/courses', getCourses);
+router.post('/courses', createCourse);
+
+// Game routes
+router.get('/games', getGames);
+router.post('/games', createGame);
 
 module.exports = router;
