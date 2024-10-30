36ab6f2d46316f0dbf548f072f573ce2e2bf7778
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-10-30 14:06:53 -0500

    Added ColorGrid Component for user input in /course page. Colors are taken from /utlis in client directory

---
+++ client/src/components/ColorGrid.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import colors from "../utils/colors";
+import "../style/ColorGrid.css";
+
+const ColorGrid = ({ onSelectColor }) => {
+    const [selectedColor, setSelectedColor] = useState("");
+
+    const handleColorClick = (colorName) => {
+        console.log("ColorGrid handleColorClick colorName:", colorName);
+        setSelectedColor(colorName);    
+        onSelectColor(colorName); 
+
+    };
+
+    return (
+        <div className="color-grid">
+            {Object.entries(colors).map(([colorName, shades]) => (
+                <div 
+                    key={colorName} 
+                    className="color-swatch" 
+                    onClick={() => handleColorClick(colorName)}
+                    style={{
+                        boxShadow: selectedColor === colorName ? `0 0 10px ${shades.dark}` : 'none',
+                    }}>
+                    <div
+                        className="color-half color-light"
+                        style={{ backgroundColor: shades.light }}
+                    />
+                    <div
+                        className="color-half color-dark"
+                        style={{ backgroundColor: shades.dark }}
+                    />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ColorGrid;
