c9ed0f8f57f94e4b56b2b655fcaf9ca621aee87a
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-10-30 12:21:53 -0500

    Color Scheme added in client utility folder

---
+++ client/src/utils/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+        "Blue": { "light": "#AFF0FF", "dark": "#0087A5" },
+        "Green": { "light": "#B8E5A0", "dark": "#008705" },
+        "Purple": { "light": "#E2BEF3", "dark": "#7302A8" },
+        "Orange": { "light": "#FFDDBD", "dark": "#C25D00" },
+        "Yellow": { "light": "#FFFCB2", "dark": "#E5DB00" },
+        "Pink": { "light": "#FFD6E7", "dark": "#CB0156" },
+        "Red": { "light": "#FFC0C0", "dark": "#A70000" },
+        "Black": { "light": "#E5E5E5", "dark": "#000000" }
+}
+
+module.exports = colors;
