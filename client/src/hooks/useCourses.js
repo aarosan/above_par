db3974ad10148e7e19e951c2762c7cbfbe42ff57
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-10-30 12:38:35 -0500

    useCourses hook for fetching Users course data on the Home Page

---
+++ client/src/hooks/useCourses.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+const apiUrl = process.env.REACT_APP_HEROKU_URL || 'http://localhost:5000';
+
+export const useCourses = () => {
+    const [courses, setCourses] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            setLoading(true);
+            const token = localStorage.getItem('token');
+            try {
+                const response = await fetch(`${apiUrl}/api/users/courses`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                });
+
+                if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+                const courseData = await response.json();
+                console.log('courseData', courseData);
+                setCourses(courseData);
+                setError(null);
+            } catch (error) {
+                setError(`Failed to fetch courses: ${error}`);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchCourses();
+    }, []);
+
+    return { courses, error, loading, setCourses};
+};
